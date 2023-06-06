3d48b816b3b8d946adab777e5d5f913db7ba5699
Oleksandr Rybka <olecksandr.rybka@ukr.net>
2023-06-07 00:39:30 +0300

    Remade admin page UI using C++.

--- client/adminpage.cpp
+++ client/adminpage.cpp
@@ -7,16 +7,182 @@ AdminPage::AdminPage(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    this->resize(620, 579);
     this->setWindowTitle("Sports Hub");
+    this->setStyleSheet("QWidget {background-color: rgb(255, 255, 255); }");
 
-    QString imagePath = QCoreApplication::applicationDirPath() + "img/switchIcon.png";
+    initializeLayouts(); // Widgets initialization
+    initializeButton();
+    initializeSpacers();
 
-    imagePath.replace("build-client-Desktop_Qt_6_5_0_MinGW_64_bit-Debug", "");
+    topHorizontalLayout(); // Layouts creation
+    configurationHorizontalLayout();
+    menuHorizontalLayout();
+    itemsMenuVerticalLayout();
 
-    ui->switchPushButton->setIcon(QPixmap(imagePath));
+    primaryContentArea(); // Content area creation
+
+    localNavigationLayout->addLayout(itemsMenuVLayout.get()); // Generic grid layout forming
+    localNavigationLayout->addSpacerItem(localNavigationSpacer_1.get());
+    localNavigationLayout->addWidget(contentArea.get());
+
+    genericGridLayout->addLayout(topHLayout.get(), 0, 0); // Generic grid layout forming
+    genericGridLayout->addLayout(configurationHLayout.get(), 1, 0);
+    genericGridLayout->addLayout(menuHLayout.get(), 2, 0);
+    genericGridLayout->addLayout(localNavigationLayout.get(), 3, 0);
 }
 
 AdminPage::~AdminPage()
 {
     delete ui;
+}
+
+void AdminPage::initializeLayouts()
+{
+    genericGridLayout = std::make_unique<QGridLayout>(this);
+
+    headerVLayout = std::make_unique<QVBoxLayout>();
+    topHLayout = std::make_unique<QHBoxLayout>();
+    configurationHLayout = std::make_unique<QHBoxLayout>();
+    menuHLayout = std::make_unique<QHBoxLayout>();
+    localNavigationLayout = std::make_unique<QHBoxLayout>();
+    itemsMenuVLayout = std::make_unique<QVBoxLayout>();
+}
+
+void AdminPage::initializeButton()
+{
+    // Top layout
+    homepagePushButton = std::make_unique<QPushButton>();
+    switchPushButton = std::make_unique<QPushButton>();
+    adminPushButton = std::make_unique<QPushButton>();
+    // Configuration layout
+    cancelPushButton = std::make_unique<QPushButton>();
+    saveChangesButton = std::make_unique<QPushButton>();
+    // Menu layout
+    homePushButton = std::make_unique<QPushButton>();
+    // Items menu layout
+    pushButton_1 = std::make_unique<QPushButton>();
+    pushButton_2 = std::make_unique<QPushButton>();
+    pushButton_3 = std::make_unique<QPushButton>();
+    pushButton_4 = std::make_unique<QPushButton>();
+    pushButton_5 = std::make_unique<QPushButton>();
+    pushButton_6 = std::make_unique<QPushButton>();
+    pushButton_7 = std::make_unique<QPushButton>();
+    pushButton_8 = std::make_unique<QPushButton>();
+    pushButton_9 = std::make_unique<QPushButton>();
+}
+
+void AdminPage::initializeSpacers()
+{
+    // Top layout
+    topSpacer_1 = std::make_unique<QSpacerItem>(0, 0, QSizePolicy::Expanding, QSizePolicy::Fixed);
+    topSpacer_2 = std::make_unique<QSpacerItem>(10, 10, QSizePolicy::Fixed, QSizePolicy::Fixed);
+    // Configuration layout
+    configSpacer_1 = std::make_unique<QSpacerItem>(60, 20, QSizePolicy::Fixed, QSizePolicy::Fixed);
+    configSpacer_2 = std::make_unique<QSpacerItem>(0, 0, QSizePolicy::Expanding, QSizePolicy::Fixed);
+    configSpacer_3 = std::make_unique<QSpacerItem>(9, 20, QSizePolicy::Fixed, QSizePolicy::Fixed);
+    // Menu layout
+    menuSpacer_1 = std::make_unique<QSpacerItem>(60, 20, QSizePolicy::Fixed, QSizePolicy::Fixed);
+    menuSpacer_2 = std::make_unique<QSpacerItem>(0, 0, QSizePolicy::Expanding, QSizePolicy::Fixed);
+    // Items menu layout
+    itemsMenuSpacer_1 = std::make_unique<QSpacerItem>(20, 30, QSizePolicy::Fixed, QSizePolicy::Fixed);
+    // Primary content area
+    localNavigationSpacer_1 = std::make_unique<QSpacerItem>(15, 20, QSizePolicy::Fixed, QSizePolicy::Fixed);
+}
+
+void AdminPage::topHorizontalLayout()
+{
+    QString imagePath = QCoreApplication::applicationDirPath() + "img/switchIcon.png";
+    QFont homepagePushButtonFont("Tahoma", 12);
+
+    imagePath.replace("build-client-Desktop_Qt_6_5_0_MinGW_64_bit-Debug", "");
+
+    homepagePushButton->setText("Sports Hub");
+    homepagePushButton->setFixedSize(150, 50);
+    homepagePushButton->setFont(homepagePushButtonFont);
+    homepagePushButton->setStyleSheet("QPushButton {background-color: rgb(208, 0, 0); color: rgb(255, 255, 255); border:none;}");
+
+    switchPushButton->setStyleSheet("QPushButton {border: none; background-color: rgb(216, 223, 233); margin-left: 10px;}");
+    switchPushButton->setIcon(QIcon("C:/Development/Projects/tech_course_project/img/switchIcon.png"));
+    switchPushButton->setIconSize(QSize(35, 40));
+
+    adminPushButton->setStyleSheet("QPushButton {background-color: rgb(255, 255, 255);}");
+    adminPushButton->setText("\"Admin\"");
+    adminPushButton->setFixedSize(80, 40);
+
+    topHLayout->addWidget(homepagePushButton.get());
+    topHLayout->addSpacerItem(topSpacer_1.get());
+    topHLayout->addWidget(switchPushButton.get());
+    topHLayout->addSpacerItem(topSpacer_2.get());
+    topHLayout->addWidget(adminPushButton.get());
+}
+
+void AdminPage::configurationHorizontalLayout()
+{
+    cancelPushButton->setText("\"Cancel\"");
+    cancelPushButton->resize(56, 20);
+    cancelPushButton->setStyleSheet("QPushButton {background-color: rgb(255, 255, 255); color: rgb(255, 0, 0); border:none}");
+
+    saveChangesButton->setText("\"Savechanges\"");
+    saveChangesButton->setFixedSize(80, 30);
+    saveChangesButton->setStyleSheet("QPushButton {background-color: rgb(208, 0, 0); color: rgb(255, 255, 255); border:none}");
+
+    QFont homeLabelFont("Tahoma", 16, QFont::Bold);
+
+    homeLabel = std::make_unique<QLabel>();
+
+    homeLabel->setText("Home");
+    homeLabel->setFont(homeLabelFont);
+
+    configurationHLayout->addSpacerItem(configSpacer_1.get());
+    configurationHLayout->addWidget(homeLabel.get());
+    configurationHLayout->addSpacerItem(configSpacer_2.get());
+    configurationHLayout->addWidget(cancelPushButton.get());
+    configurationHLayout->addSpacerItem(configSpacer_3.get());
+    configurationHLayout->addWidget(saveChangesButton.get());
+}
+
+void AdminPage::menuHorizontalLayout()
+{
+    QFont homePushButtonFont("Tahoma", 10, QFont::Bold);
+
+    homePushButton->setFont(homePushButtonFont);
+    homePushButton->setText("HOME");
+    homePushButton->setStyleSheet("QPushButton {border: none; background-color: rgb(255, 255, 255); color: rgb(255, 0, 0);}");
+
+    menuHLayout->addSpacerItem(menuSpacer_1.get());
+    menuHLayout->addWidget(homePushButton.get());
+    menuHLayout->addSpacerItem(menuSpacer_2.get());
+}
+
+void AdminPage::itemsMenuVerticalLayout()
+{
+    pushButton_1->setStyleSheet("margin-bottom: 5px;");
+    pushButton_2->setStyleSheet("margin-bottom: 5px;");
+    pushButton_3->setStyleSheet("margin-bottom: 5px;");
+    pushButton_4->setStyleSheet("margin-bottom: 5px;");
+    pushButton_5->setStyleSheet("margin-bottom: 5px;");
+    pushButton_6->setStyleSheet("margin-bottom: 5px;");
+    pushButton_7->setStyleSheet("margin-bottom: 5px;");
+    pushButton_8->setStyleSheet("margin-bottom: 5px;");
+    pushButton_9->setStyleSheet("margin-bottom: 5px;");
+
+    itemsMenuVLayout->addWidget(pushButton_1.get());
+    itemsMenuVLayout->addWidget(pushButton_2.get());
+    itemsMenuVLayout->addWidget(pushButton_3.get());
+    itemsMenuVLayout->addWidget(pushButton_4.get());
+    itemsMenuVLayout->addWidget(pushButton_5.get());
+    itemsMenuVLayout->addWidget(pushButton_6.get());
+    itemsMenuVLayout->addWidget(pushButton_7.get());
+    itemsMenuVLayout->addWidget(pushButton_8.get());
+    itemsMenuVLayout->addWidget(pushButton_9.get());
+
+    itemsMenuVLayout->addSpacerItem(itemsMenuSpacer_1.get());
+}
+
+void AdminPage::primaryContentArea()
+{
+    contentArea = std::make_unique<QScrollArea>();
+
+    contentArea->setStyleSheet("QScrollArea {border-top: 1px solid white; border-left: 2px solid rgb(211, 211, 211); border-bottom: white; border-right: white;}");
 }
--- client/adminpage.h
+++ client/adminpage.h
@@ -2,7 +2,15 @@
 #define ADMINPAGE_H
 
 #include <QWidget>
+#include <QCoreApplication>
+#include <QGridLayout>
 #include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QSpacerItem>
+#include <QPushButton>
+#include <QLabel>
+#include <QScrollArea>
+#include <memory>
 
 namespace Ui {
 class AdminPage;
@@ -18,6 +26,56 @@ public:
 
 private:
     Ui::AdminPage *ui;
+
+    std::unique_ptr<QGridLayout> genericGridLayout;
+
+    std::unique_ptr<QVBoxLayout> headerVLayout;
+    std::unique_ptr<QVBoxLayout> itemsMenuVLayout;
+
+    std::unique_ptr<QHBoxLayout> topHLayout;
+    std::unique_ptr<QHBoxLayout> configurationHLayout;
+    std::unique_ptr<QHBoxLayout> menuHLayout;
+    std::unique_ptr<QHBoxLayout> localNavigationLayout;
+
+    std::unique_ptr<QSpacerItem> topSpacer_1;
+    std::unique_ptr<QSpacerItem> topSpacer_2;
+    std::unique_ptr<QSpacerItem> configSpacer_1;
+    std::unique_ptr<QSpacerItem> configSpacer_2;
+    std::unique_ptr<QSpacerItem> configSpacer_3;
+    std::unique_ptr<QSpacerItem> menuSpacer_1;
+    std::unique_ptr<QSpacerItem> menuSpacer_2;
+    std::unique_ptr<QSpacerItem> itemsMenuSpacer_1;
+    std::unique_ptr<QSpacerItem> localNavigationSpacer_1;
+    std::unique_ptr<QPushButton> homepagePushButton;
+    std::unique_ptr<QPushButton> switchPushButton;
+    std::unique_ptr<QPushButton> adminPushButton;
+    std::unique_ptr<QPushButton> cancelPushButton;
+    std::unique_ptr<QPushButton> saveChangesButton;
+    std::unique_ptr<QPushButton> homePushButton;
+    std::unique_ptr<QPushButton> pushButton_1;
+    std::unique_ptr<QPushButton> pushButton_2;
+    std::unique_ptr<QPushButton> pushButton_3;
+    std::unique_ptr<QPushButton> pushButton_4;
+    std::unique_ptr<QPushButton> pushButton_5;
+    std::unique_ptr<QPushButton> pushButton_6;
+    std::unique_ptr<QPushButton> pushButton_7;
+    std::unique_ptr<QPushButton> pushButton_8;
+    std::unique_ptr<QPushButton> pushButton_9;
+
+    std::unique_ptr<QLabel> homeLabel;
+
+    std::unique_ptr<QScrollArea> contentArea;
+
+    void initializeLayouts();
+    void initializeButton();
+    void initializeSpacers();
+
+    void topHorizontalLayout();
+    void configurationHorizontalLayout();
+    void menuHorizontalLayout();
+    void itemsMenuVerticalLayout();
+
+    void primaryContentArea();
 };
 
 #endif // ADMINPAGE_H
